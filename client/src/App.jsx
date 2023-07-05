f7d619af57258994053c8fd4899e66b8e26338b3
Awais Aslam <awaisaslam336@gmail.com>
2023-07-05 21:15:12 +0500

    tailwind css for vite setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Chat App</h1>
+      <h1 className="text-3xl font-bold underline">Chat App</h1>
       <p>Get cracking with your new project!</p>
     </div>
   );
